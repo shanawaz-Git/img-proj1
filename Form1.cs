f5e07fcb977a0b0e3914bb7b254d6e735324933e
shanawaz-Git <98632192+shanawaz-Git@users.noreply.github.com>
2022-03-24 16:05:52 +0530

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+ 
+namespace img_pro
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection con;
+        OleDbCommand cmd;
+        OleDbDataReader dr;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string usr = textBox1.Text; 
+            string psw = textBox2.Text;
+            con = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:/Users/eve2/Documents/d1.accdb");
+            cmd = new OleDbCommand();
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "SELECT * FROM lofin where userid='"+textBox1.Text + "' AND passcode='" + textBox2.Text + "'";
+            dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                Form2 f2 = new Form2();
+                f2.Show();
+                Hide();
+            }
+            else
+            {
+                label3.Text = "invalid credentials..";
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form3 f3 = new Form3();
+            f3.Show();
+            Hide();
+        }
+       
+    }
+}
